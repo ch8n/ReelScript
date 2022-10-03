eb7597f6cd7a5b87034e8f93e1c19257341efb23
ch8n <chetan.garg36@gmail.com>
2022-10-03 17:35:19 +0530

    script for quote generator

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,72 +1,88 @@
 import com.sksamuel.scrimage.ImmutableImage
-import com.sksamuel.scrimage.Position
-import com.sksamuel.scrimage.composite.AlphaComposite
-import com.sksamuel.scrimage.composite.ColorBurnComposite
-import com.sksamuel.scrimage.composite.ColorComposite
-import com.sksamuel.scrimage.composite.GlowComposite
-import com.sksamuel.scrimage.composite.LuminosityComposite
-import com.sksamuel.scrimage.composite.OverlayComposite
-import com.sksamuel.scrimage.composite.RedComposite
-import com.sksamuel.scrimage.composite.SaturationComposite
-import com.sksamuel.scrimage.composite.ScreenComposite
-import com.sksamuel.scrimage.composite.SubtractComposite
-import com.sksamuel.scrimage.filter.AlphaMaskFilter
-import com.sksamuel.scrimage.filter.BlurFilter
-import com.sksamuel.scrimage.filter.BorderFilter
 import com.sksamuel.scrimage.nio.JpegWriter
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.coroutineScope
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import java.awt.*
 import java.awt.font.TextAttribute
-import java.awt.image.BufferedImage
 import java.io.File
 import java.io.InputStream
 import java.text.AttributedString
+import java.util.UUID
 import kotlin.math.roundToInt
 
 
 const val DIRECTORY_OUTPUT = "output"
 
-suspend fun main() = coroutineScope {
+
+suspend fun main() {
+    coroutineScope {
+        val data = mapOf(
+            Pair(
+                "https://images.unsplash.com/photo-1518837321959-58dfc718abcf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+                "Here strange creatures are watching the kotlin logo. You can drag'n'drop them as well as the logo. Doubleclick to add more creatures but be careful. They may be watching you!"
+            ),
+        )
+        data.map { (url, quote) ->
+            async {
+                createQuoteImageOrNull(
+                    imageUrl = url,
+                    quote = quote
+                )?.write(UUID.randomUUID().toString())
+            }
+        }.awaitAll()
+    }
+}
+
+suspend fun createQuoteImageOrNull(
+    imageUrl: String,
+    quote: String,
+    frameSize: Size = Size(1080, 1920),
+    fontSize: Int = 52,
+): ImmutableImage? = runCatching {
     resetDirectory()
 
     println("Getting images!")
-    val imageUrl =
-        "https://images.unsplash.com/photo-1518837321959-58dfc718abcf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-    val imageStream = getImageStreamOrNull(imageUrl) ?: return@coroutineScope
-    val image = ImmutableImage.loader().fromStream(imageStream)
-    println("${image.width} X ${image.height} | ${image.ratio()}")
+    val imageStream = getImageStreamOrNull(imageUrl) ?: return null
 
-    println("Cropping to reel size!")
-    val reelImageSize = Size(1080, 1920)
-    val imageCropped = image.cropSize(reelImageSize)
-    println("${imageCropped.width} X ${imageCropped.height} | ${imageCropped.ratio()}")
-    imageCropped.write("result1")
+    println("Cropping to size!")
+    val originalImage = ImmutableImage.loader().fromStream(imageStream)
+    println("Original : ${originalImage.width} X ${originalImage.height} | ${originalImage.ratio()}")
+
+    val imageCropped = originalImage.cropSize(frameSize)
+    println("Cropped : ${imageCropped.width} X ${imageCropped.height} | ${imageCropped.ratio()}")
 
     println("Appending Quote")
-    val quote =
-        "Here strange creatures are watching the kotlin logo. You can drag'n'drop them as well as the logo. Doubleclick to add more creatures but be careful. They may be watching you!"
-
     val awtImage = imageCropped.awt()
     val imageWithQuote = with(awtImage) {
         val graphics = graphics
-        val font = Font(Font.MONOSPACED, Font.BOLD, 52)
+        val font = Font(Font.MONOSPACED, Font.BOLD, fontSize)
         val fontMetrics: FontMetrics = graphics.getFontMetrics(font)
 
-        val lines = quote.getBoundFormattedLines(reelImageSize.center, fontMetrics)
+        val lines = quote.getBoundFormattedLines(frameSize.center, fontMetrics)
         val maxWidth = lines.maxOf { fontMetrics.stringWidth(it) }
         val height = fontMetrics.height * lines.size
         val quoteSize = Size(width = maxWidth, height = height)
 
-        var yLoc = (reelImageSize.center.height - quoteSize.center.height) + fontMetrics.ascent
-        val xLoc = reelImageSize.center.width - quoteSize.center.width
+        var yLoc = (frameSize.center.height - quoteSize.center.height) + fontMetrics.ascent
+        val xLoc = frameSize.center.width - quoteSize.center.width
 
         lines.forEach { line ->
             val lineWidth = fontMetrics.stringWidth(line)
             val attributedText = AttributedString(line)
             attributedText.addAttribute(TextAttribute.FONT, font)
             attributedText.addAttribute(TextAttribute.FOREGROUND, Color.WHITE)
+            graphics.color = Color.BLACK
+            graphics.fill3DRect(
+                xLoc + (fontMetrics.ascent * 0.25).roundToInt(),
+                yLoc - fontMetrics.ascent,
+                lineWidth,
+                fontMetrics.height,
+                true
+            )
+            graphics.color = Color.WHITE
             graphics.drawRoundRect(
                 xLoc + (fontMetrics.ascent * 0.25).roundToInt(),
                 yLoc - fontMetrics.ascent,
@@ -81,9 +97,8 @@ suspend fun main() = coroutineScope {
 
         ImmutableImage.fromAwt(this)
     }
-    imageWithQuote.write("result2")
-    Unit
-}
+    return imageWithQuote
+}.getOrNull()
 
 data class Size(val width: Int, val height: Int) {
     val center get() = this.copy(width = width / 2, height = height / 2)
